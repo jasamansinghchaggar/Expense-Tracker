63d4b7908da45c7c1ce41a1a1342b210e118b0b3
Jasaman Singh <you@example.com>
2024-06-07 21:46:52 +0530

    expense tracker site uploaded

---
+++ main.js
@@ -0,0 +1,116 @@
+// For feather icon
+
+feather.replace();
+
+// Expense tracker working
+
+// Elements of the popup
+
+let expensePopup = document.getElementById("expense-popup");
+let crossIcon = document.getElementById("cross");
+let expenseTitle = document.getElementById("title");
+let expenseAmount = document.getElementById("amount");
+let expenseDate = document.getElementById("date");
+let submit = document.getElementById("submit");
+
+// Elements of the landing page
+
+let expenseContainer = document.getElementById("expense-container");
+let entryButton = document.getElementById("expense-entry-button");
+let totalAmount = document.getElementById("expense-total");
+let expenseList = document.querySelector(".expense-list");
+
+
+// Working of the popup view
+
+entryButton.addEventListener("click", function() {
+    expensePopup.classList.remove("hide");
+    expenseContainer.style.opacity = "0.5";
+})
+
+crossIcon.addEventListener("click", function() {
+    expensePopup.classList.add("hide");
+    expenseContainer.style.opacity = "1";
+})
+
+// Form functionality
+
+let expenseForm = document.querySelector("form");
+
+expenseForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let title = expenseTitle.value;
+    let amount = expenseAmount.value;
+    let rawDate = expenseDate.value;
+
+    let formattedDate = moment(rawDate).format("MM/DD/YY");
+
+    let expenseData = {
+        title: title,
+        amount: amount,
+        date: formattedDate,
+    };
+
+    // Create a new list item
+
+    let newExpenseItem = document.createElement("li");
+
+    newExpenseItem.classList.add("expense");
+
+    // Create inner HTML for the new list item
+
+    newExpenseItem.innerHTML = `
+        <div class="expense-details">
+            <p class="expense-title">${expenseData.title}</p>
+            <p class="expense-date">Paid on ${expenseData.date}</p>
+        </div>
+        <p class="expense-amount">- $${expenseData.amount}</p>
+    `;
+
+    // Append the list item to the ul
+
+    expenseList.appendChild(newExpenseItem);
+
+    // Reset the form fields
+    expenseForm.reset();
+
+    // Close the expense popup
+
+    expensePopup.classList.add("hide");
+    expenseContainer.style.opacity = "1";
+    
+    updateExpenseOverview(expenseData.amount);
+    emptyListMessage();
+
+})
+
+// update total expense amount
+
+let total = 0;
+
+function updateExpenseOverview(amount) {
+    total += parseFloat(amount);
+    totalAmount.innerText = "₹" + total.toFixed(2);
+}
+
+// Empty list Message
+
+function emptyListMessage() {
+    let message = "No expenses recorded!"
+    let messageNode = document.createElement("li");
+    messageNode.classList.add("list-info");
+    messageNode.textContent = message;
+
+    if(expenseList.children.length === 0 ){
+        expenseList.appendChild(messageNode);
+    }
+    else {
+        let existingMessage = expenseList.querySelector(".list-info");
+        if(existingMessage) {
+            expenseList.removeChild(existingMessage);
+        }
+    }
+}
+
+emptyListMessage();
